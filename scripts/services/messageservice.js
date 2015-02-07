18c8610659491d3490ad40c60af0bc8d0a5cb64b
Doug Sebastian <dasebast@gmail.com>
2015-02-06 17:46:19 -0700

    chatty review done

--- scripts/services/messageservice.js
+++ scripts/services/messageservice.js
@@ -5,13 +5,13 @@
         .factory('MessageService', function MessageService($http) {
             return {
                 getMessages: function() {
-                    return $http.get('http://localhost:9901')
+                    return $http.get('http://localhost:9001')
                         .then(function(response) {
                             return response.data;
                         });
                 },
                 postMessage: function(message){
-                	return $http.post('http://localhost:9901', {text: message})
+                	return $http.post('http://localhost:9001', {text: message})
                 		.then(function(response){
                 			return response.data;
                 		});
--- server.js
+++ server.js
@@ -1,49 +1,92 @@
-var http = require('http');
-var port = 9901;
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 9001;
+app.listen(port);
+
 var messages = [];
 
+app.use(bodyParser.json());
 
-var onRequest = function(req, res) {
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.send(messages); //check to see if bodyparser does stringify automatically
+});
+
+app.post('/', function(req, res) {
+    req.body.createdAt = new Date();
+    messages.push(req.body);
+    res.send(req.body);
+});
 
 
-    if (req.method === 'GET') {
-        res.writeHead(200, {
-            'Connection': 'close',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            // 'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-        });
-        res.end(JSON.stringify(messages));
-    } else if (req.method === 'POST') {
-        var postData = '';
-        req.on('data', function(chunk) {
-            postData += chunk;
-        });
-        req.on('end', function() {
-            console.log("Got POST data:");
-            var newMessage = JSON.parse(postData);
-            newMessage.createdAt = new Date().toISOString();
-            messages.push(newMessage);
+// app.get('/', function(req, res) {
+//     if(req.method == 'GET') {
+//         res.end(JSON.stringify({messages}));
+//     };
+//     if(req.method === 'POST') {
+//         res.
+//     }
+// });
 
-            res.writeHead(200, {
-                'Connection': 'close',
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-            });
-            res.end(JSON.stringify(messages));
 
-        });
 
-    } else if (req.method === 'OPTIONS') {
-        res.writeHead(200, {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        });
-        res.end();
-    }
-};
 
-http.createServer(onRequest).listen(port);
-console.log('Listening on port 9901');
+
+
+
+// var http = require('http');
+// var port = 9901;
+// var messages = [];
+
+
+// var onRequest = function(req, res) {
+
+
+//     if (req.method === 'GET') {
+//         res.writeHead(200, {
+//             'Connection': 'close',
+//             'Content-Type': 'application/json',
+//             'Access-Control-Allow-Origin': '*',
+//             // 'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//         });
+//         res.end(JSON.stringify(messages));
+//     } else if (req.method === 'POST') {
+//         var postData = '';
+//         req.on('data', function(chunk) {
+//             postData += chunk;
+//         });
+//         req.on('end', function() {
+//             console.log("Got POST data:");
+//             var newMessage = JSON.parse(postData);
+//             newMessage.createdAt = new Date().toISOString();
+//             messages.push(newMessage);
+
+//             res.writeHead(200, {
+//                 'Connection': 'close',
+//                 'Content-Type': 'application/json',
+//                 'Access-Control-Allow-Origin': '*',
+//                 'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//             });
+//             res.end(JSON.stringify(messages));
+
+//         });
+
+//     } else if (req.method === 'OPTIONS') {
+//         res.writeHead(200, {
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+//         });
+//         res.end();
+//     }
+// };
+
+// http.createServer(onRequest).listen(port);
+// console.log('Listening on port 9901');
